9df17018c4dae4bfcf25e7f7203518580936fcf8
BorosBalazsLaszlo <c9borbal@vasvari.org>
2025-02-17 09:32:40 +0100

    database alapja kész

---
+++ backend/database.js
@@ -0,0 +1,38 @@
+import sqlite3 from 'sqlite3';
+
+const db = new sqlite3.Database("./database.db");
+
+const DbInit = async () =>{
+    await DbRun(
+        "CREATE TABLE IF NOT EXISTS Novenyek (id INTEGER PRIMARY KEY AUTOINCREMENT ,nev text, evelo bool, kategoria text, ar integer)"
+    )
+};
+
+function DbQuery(sql, params) {
+    return new Promise((resolve, reject) =>{
+        db.all(sql, params, (err, rows) =>{
+            if (err)
+            {
+                reject(err);
+            }
+            else{
+                resolve(rows);
+            }
+        });
+    });
+}
+
+function DbRun(sql, params){
+    return new Promise((resolve, reject) =>{
+        db.run(sql, params, function (err){
+            if(err)
+            {
+                reject(err);
+            }
+            else
+            {
+                resolve({ id: this.lastID});
+            }
+        });
+    });
+}
